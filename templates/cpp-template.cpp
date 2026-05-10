44294f3f2794e9d992c3b2e13809794cded534ed
shrrain-cs <2985292201@qq.com>
2026-05-10 20:55:23 +0800

    Create cpp-template.cpp with initial setup
    Add a C++ template file with basic structure

---
+++ templates/cpp-template.cpp
@@ -0,0 +1,22 @@
+/**
+* @file
+ * @author shrrain-cs
+ * @brief
+ * @version 1.0
+ * @date
+ *
+ * 算法思路：
+ * 1.
+ * 2.
+ * 3.
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);cout.tie(nullptr);
+
+	return 0;
+}
